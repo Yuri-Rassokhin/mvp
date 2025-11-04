2a68cdb04d2a2d905c53ecea1d0f1c883b487559
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2025-11-04 09:18:20 +0000

    debug: database URL is read from ~/.mvp/database

---
+++ src/mesh.py
@@ -0,0 +1,77 @@
+import socket
+from pathlib import Path
+import json
+
+def find_free_port(start=8500, end=8999):
+    for port in range(start, end + 1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(("", port))
+                return port
+            except OSError:
+                continue
+    raise RuntimeError("no free port found in the range")
+
+
+
+def update_component_status(name, description, endpoints, port, module, instance_id):
+    status_path = Path.home() / ".mvp" / "status"
+    status_path.parent.mkdir(parents=True, exist_ok=True)
+
+    if status_path.exists():
+        try:
+            with open(status_path, "r") as f:
+                status = json.load(f)
+            if not isinstance(status, list):
+                status = []
+        except:
+            status = []
+    else:
+        status = []
+
+    # Получаем сигнатуры входов/выходов для функций из этого компонента
+    io_signatures = {}
+    for fname in endpoints:
+        func = getattr(module, fname, None)
+        if not func:
+            continue
+
+        sig = inspect.signature(func)
+
+        args = {
+            k: getattr(v.annotation, "__name__", str(v.annotation)) if v.annotation != inspect._empty else "str"
+# this option disallows List and such
+#            k: str(v.annotation.__name__) if v.annotation != inspect._empty else "str"
+            for k, v in sig.parameters.items()
+        }
+
+        io_signatures[fname] = {
+            "inputs": args,
+            "returns": "JSON"
+        }
+
+    for entry in status:
+        if entry.get("id") == instance_id:
+            entry.update({
+                "name": name,
+                "description": description,
+                "endpoints": endpoints,
+                "port": port,
+                "ip": socket.gethostbyname(socket.gethostname()),
+                "io": io_signatures
+            })
+            break
+    else:
+        status.append({
+            "id": instance_id,
+            "name": name,
+            "description": description,
+            "endpoints": endpoints,
+            "port": port,
+            "ip": socket.gethostbyname(socket.gethostname()),
+            "io": io_signatures
+        })
+
+    with open(status_path, "w") as f:
+        json.dump(status, f, indent=2)
+
